8603f9accd1d023fac87a0afd362b750488fcb57
ShuranJia <111563579+ShuranJia@users.noreply.github.com>
2022-08-19 17:49:23 +0800

    Update team.js

--- data/team.js
+++ data/team.js
@@ -3,84 +3,84 @@ var team = [
         id: 1,
         name: "黄礼东",
         description: "xxxx",
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/黄礼东.png",
         role: "Coach"
     },
     {
         id: 2,
         name: "黄可",
         description: "xxxx",
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/黄可.png",
         role: "Coach"
     },
     {
         id: 3,
         name: "张乃天 Neal",
         position: ["First base", "Second base"],
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/张乃天.png",
         role: "Captain"
     },
     {
         id: 4,
         name: "张健宁 Harris",
         position: "Shortstop",
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/张健宁.png",
         role: "Vice-captain"
     },
     {
         id: 5,
         name: "杨嘉镒 Joey",
         position: ["Second base", "Third base"],
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/杨嘉y.png",
         role: "Player"
     },
     {
         id: 6,
         name: "郑好童 Thor",
         position: ["Pitcher", "Center fielder"],
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/郑好童.png",
         role: "Player"
     },
     {
         id: 7,
         name: "贾舒然 Susan",
         position: "Catcher",
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/贾舒然.png",
         role: "Player"
     },
     {
         id: 8,
         name: "陈芊贝",
         position: "Right fielder",
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/陈芊贝.png",
         role: "Player"
     },
     {
         id: 9,
         name: "陈框泽 Ocean",
         position: "Right fielder",
-        image: "https://picsum.photos/400/400",
+        image: "http://picsum.photos/200/200",
         role: "Player"
     },
     {
         id: 10,
         name: "钟皓 Hason",
         position: "Pitcher",
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/钟皓.png",
         role: "Player"
     },
     {
         id: 11,
         name: "贺厚杰 Tommy",
         position: ["First base", "Third base"],
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/贺厚杰.png",
         role: "Player"
     },
     {
         id: 12,
         name: "周健宇 Collin",
         position: "Left fielder",
-        image: "https://picsum.photos/400/400",
+        image: "./img.png/周健宇.png",
         role: "Player"
     },
 
